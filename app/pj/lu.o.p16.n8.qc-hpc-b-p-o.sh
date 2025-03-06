5fa2a9b32c820717ac1fb7aa8a97d8aa59267d2f
smallcat9603 <huyao0107@gmail.com>
2025-03-07 00:00:40 +0900

    minor

---
+++ app/pj/lu.o.p16.n8.qc-hpc-b-p-o.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+#------ pjsub option --------#
+#PJM -L rscgrp=qc-hpc-b-p-o
+#PJM -L node=2x4:torus
+#PJM --mpi proc=16
+#PJM -L elapse=1:00:00
+#PJM -g gz00
+#PJM -j
+
+#------- Program execution -------#
+mpiexec /work/gz00/z30130/qcsched/app/bin/lu.o 1024
